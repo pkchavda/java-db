5d1cfcbe6f50e7c7849de21f535292cee7f642f1
Parth Chavda <parthkchavda007@gmail.com>
2026-06-17 20:55:21 +0530

    Java Switch case add
    user Selet the 1 and 2 number option in java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -31,6 +31,7 @@
             Data data = new Data();
 
             System.out.print("Enter your name: ");
+            scanner.nextLine();
             data.name = scanner.nextLine();
 
             System.out.print("Enter your age: ");
@@ -47,47 +48,72 @@
         }
 
         static String addQuestion() {
-            System.out.println("Enter your question: ");
-            scanner.nextLine(); 
-            String question = scanner.nextLine();
+                System.out.println("Enter your question: ");
+                String question = scanner.nextLine();
 
-            System.out.println("Question: " + question);
+                System.out.println("Question: " + question);
 
-            return question;
+                return question;
         }
 
         public static void main(String[] args) throws SQLException {
 
+
+            System.out.println("Welcome to the Database Connection Program! \n Please select an option: ");
+            System.out.println("1. Add Student: ");
+            System.out.println("2. Add Question: ");
+
+            int choice = scanner.nextInt();
+
             Connection conn = connectToDatabase();
-            if (conn == null) {
-                System.out.println("Failed to connect to database.");
-                return;
+
+            switch (choice) {
+            
+                case 1:
+
+                    if (conn == null) {
+                        System.out.println("Failed to connect to database.");
+                        return;
+                    }
+
+                    Data data = addStudentinput();
+                    System.out.println(data.toString()); // bydefult toString()
+
+                    String sql = "insert into student (name, age, number) values (?, ?, ?)";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+
+                    pstmt.setString(1, data.name);
+                    pstmt.setInt(2, data.age);
+                    pstmt.setLong(3, data.phonenumber);
+
+                    pstmt.executeUpdate();
+                    System.out.println("Data inserted successfully!😊");
+                    
+                    scanner.close();
+
+                    break;
+
+                case 2:
+
+                    if (conn == null) {
+                        System.out.println("Failed to connect to database.");
+                        return;
+                    }
+
+                    String question = addQuestion();
+                    System.out.println("Question: " + question);
+
+                    String sql1 = "insert into question_table (question) values (?)";
+                    PreparedStatement pstmt1 = conn.prepareStatement(sql1);
+
+                    pstmt1.setString(1, question);
+                    pstmt1.executeUpdate();
+
+                    System.out.println("Question inserted successfuly");
+
+                default:
+                    System.out.println("Invalid choice! Please select either 1 or 2.");
             }
-
-            Data data = addStudentinput();
-            System.out.println(data.toString()); // bydefult toString()
-
-            String question = addQuestion();
-            System.out.println("Question: " + question);
-
-            String sql1 = "insert into question_table (question) values (?)";
-            PreparedStatement pstmt1 = conn.prepareStatement(sql1);
-            pstmt1.setString(1, question);
-            pstmt1.executeUpdate();
-
-            System.out.println("Question inserted successfuly");
-
-            String sql = "insert into student (name, age, number) values (?, ?, ?)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            pstmt.setString(1, data.name);
-            pstmt.setInt(2, data.age);
-            pstmt.setLong(3, data.phonenumber);
-
-            pstmt.executeUpdate();
-            System.out.println("Data inserted successfully!😊");
-
-            scanner.close();
         }
     }
 
