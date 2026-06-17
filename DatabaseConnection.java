28ffec1de1a9317a1d8d1f0fa3b03e1db23f50a3
Parth Chavda <parthkchavda007@gmail.com>
2026-06-17 13:00:57 +0530

    Java PreparedStatement New
    New fetur add for the sqlinjection security

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,22 +1,21 @@
+import dao.Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class DatabaseConnection {
 
-    static Statement connectToDatabase() {
+    static Connection connectToDatabase() {
         String url = "jdbc:mysql://localhost:3306/mydb";
         String username = "root";
-        String password = "";
+        String password = "123456789";
 
         try {
             Connection con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Successful!");
-            Statement stmt = con.createStatement();
-
-            return stmt;
+            return con;
 
         } catch (SQLException e) {
             System.out.println("Connection Failed!");
@@ -49,20 +48,23 @@ public class DatabaseConnection {
 
     public static void main(String[] args) throws SQLException {
 
-        Statement stmt = connectToDatabase();
-        if (stmt == null) {
-            System.out.println("Statement is null. Cannot insert data.");
+        Connection conn = connectToDatabase();
+        if (conn == null) {
+            System.out.println("Failed to connect to database.");
             return;
         }
 
         Data data = getUserinput();
         System.out.println(data.toString()); // bydefult toString()
 
-        String sql = "insert into student (name, age, number) values (data.name, data.age, data.phonenumber)";
-        System.out.println(sql);
+        String sql = "insert into student (name, age, number) values (?, ?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
 
-        stmt.executeUpdate(sql);
+        pstmt.setString(1, data.name);
+        pstmt.setInt(2, data.age);
+        pstmt.setLong(3, data.phonenumber);
+
+        pstmt.executeUpdate();
         System.out.println("Data inserted successfully!😊");
     }
-
 }
